b6b00e5b335fab26e9f3cf4721abe64f5f7b29d4
James Ly <jamesly@Jamess-MacBook-Pro.local>
2017-05-11 19:58:53 -0700

    schlicks approximation

--- prog1/src/ReflectRefract.cpp
+++ prog1/src/ReflectRefract.cpp
@@ -1,13 +1,32 @@
 #include "ReflectRefract.h"
 
+float schlicks_aprrox(float ior, glm::vec3 normal, glm::vec3 view) {
+    float F0, F, NdotV;
+    F0 = glm::pow(ior - 1.0f, 2.0f)/glm::pow(ior + 1.0f, 2.0f);
+    NdotV = glm::dot(normal, view);
+    F = F0 + (1.0f - F0) * glm::pow(1.0f - NdotV, 5.0f);
+    return F;
+}
+
+glm::vec3 beers_law(glm::vec4 objColor, float distance) {
+    float alpha, euler;
+    glm::vec3 color, absorbance, attenuation;
+    color = glm::vec3(objColor.x, objColor.y, objColor.z);
+    alpha = 0.15f;
+    euler = 2.71828f;
+    absorbance = color * alpha * -1.0f * distance;
+    attenuation = glm::vec3(glm::pow(euler, absorbance.x), glm::pow(euler, absorbance.y), glm::pow(euler, absorbance.z));
+    return attenuation;
+}
+
 glm::vec3 raytrace(glm::vec3 p0, glm::vec3 d, std::vector<GeomObj*> objList, std::vector<Light*> lightList, int depth) {
     glm::vec3 color, local, reflectColor, reflectionVec, intersectionPt, objNormal;
-    glm::vec3 refractionVec, refractionColor;
+    glm::vec3 refractionVec, refractionColor, attenuation;
     glm::vec4 objColor;
     color = glm::vec3(0, 0, 0);
     int hitNdx;
-    float t, epsilon, DdotN, n1, n2, localContribut;
-    float reflectCoeff, refractCoeff, filterVal;
+    float t, epsilon, DdotN, n1, n2, localContribut, reflectContribut, refractContribut;
+    float reflectCoeff, refractCoeff, filterVal, fresnelReflectance, ior, distance;
     Ray ray = Ray(p0, d);
     epsilon = 0.001f;
 
@@ -22,12 +41,13 @@ glm::vec3 raytrace(glm::vec3 p0, glm::vec3 d, std::vector<GeomObj*> objList, std
             reflectColor = raytrace(intersectionPt + reflectionVec * epsilon, reflectionVec, objList, lightList, depth - 1);
             reflectCoeff = (float)objList[hitNdx]->get_reflection();
             DdotN = glm::dot(d, objNormal);
+            ior = (float)objList[hitNdx]->get_ior();
             if (DdotN < 0) {
                 n1 = 1.0f;
-                n2 = objList[hitNdx]->get_ior();
+                n2 = ior;
             }
             else {
-                n1 = objList[hitNdx]->get_ior();
+                n1 = ior;
                 n2 = 1.0f;
                 objNormal = -1.0f * objNormal;
             }
@@ -37,9 +57,14 @@ glm::vec3 raytrace(glm::vec3 p0, glm::vec3 d, std::vector<GeomObj*> objList, std
             
             objColor = objList[hitNdx]->get_rgb();
             filterVal = objColor.w;
+            fresnelReflectance = schlicks_aprrox(ior, objNormal, -1.0f * d);
             localContribut = (1.0f - filterVal) * (1.0f - reflectCoeff);
+            reflectContribut = (1.0f - filterVal) * reflectCoeff + filterVal * fresnelReflectance;
+            refractContribut = filterVal * (1.0f - fresnelReflectance);
+            distance = glm::distance(p0, intersectionPt);
+            attenuation = beers_law(objColor, distance);
 
-            color += localContribut * local + reflectCoeff * reflectColor + refractCoeff * refractionColor;
+            color += localContribut * local + reflectContribut * reflectColor + refractContribut * refractionColor;
         }
     }
     return color;
--- prog1/src/ReflectRefract.h
+++ prog1/src/ReflectRefract.h
@@ -9,6 +9,8 @@
 #include "Ray.h"
 #include "Shading.h"
 
+float schlick_approx(float ior, glm::vec3 normal, glm::vec3 view);
+glm::vec3 beers_law(glm::vec4 objColor, float distance);
 glm::vec3 raytrace(glm::vec3 p0, glm::vec3 d, std::vector<GeomObj*> objList, std::vector<Light*> lightList, int depth);
 
 #endif
