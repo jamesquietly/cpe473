9b7ac761d41d9d34106d31db0d1d8bfc59b6069d
James Ly <jamesly@Jamess-MacBook-Pro.local>
2017-05-19 17:35:15 -0700

    fresnel

--- prog1/src/ReflectRefract.cpp
+++ prog1/src/ReflectRefract.cpp
@@ -26,7 +26,7 @@ glm::vec3 raytrace(glm::vec3 p0, glm::vec3 d, float *tPassBack, std::vector<Geom
     color = glm::vec3(0, 0, 0);
     int hitNdx;
     float t, epsilon, DdotN, n1, n2, localContribut, reflectContribut, refractContribut;
-    float reflectCoeff, refractCoeff, filterVal, fresnelReflectance, ior, distance, reflectT, refractT;
+    float reflectCoeff, refractCoeff, filterVal, fresnelReflectance = 0, ior, distance, reflectT, refractT;
     Ray ray = Ray(p0, d);
     epsilon = 0.001f;
     reflectT = -1.0f;
@@ -43,6 +43,9 @@ glm::vec3 raytrace(glm::vec3 p0, glm::vec3 d, float *tPassBack, std::vector<Geom
             
             //reflection
             reflectionVec = ray.calc_reflection(objNormal);
+            if (std::isnan(reflectionVec.x)) {
+                reflectionVec = glm::vec3(0, 0, 0);
+            }
             reflectCoeff = (float)objList[hitNdx]->get_reflection();
 
             //check if entering or leaving media
@@ -61,6 +64,10 @@ glm::vec3 raytrace(glm::vec3 p0, glm::vec3 d, float *tPassBack, std::vector<Geom
 
             //refraction
             refractionVec = ray.calc_refraction(objNormal, n1, n2);
+            if (std::isnan(refractionVec.x)) {
+                refractionVec = glm::vec3(0, 0, 0);
+            }
+
             refractPt = intersectionPt + refractionVec * epsilon;
             refractCoeff = (float)objList[hitNdx]->get_refraction();
             
@@ -68,7 +75,11 @@ glm::vec3 raytrace(glm::vec3 p0, glm::vec3 d, float *tPassBack, std::vector<Geom
             //set fresnel = 0 for fresnel off
             objColor = objList[hitNdx]->get_rgb();
             filterVal = objColor.w;
-            fresnelReflectance = schlicks_aprrox(ior, objNormal, -1.0f * d);
+            
+            if (useFresnel) {    
+                fresnelReflectance = schlicks_aprrox(ior, objNormal, -1.0f * d);
+            }
+
             localContribut = (1.0f - filterVal) * (1.0f - reflectCoeff);
             reflectContribut = (1.0f - filterVal) * reflectCoeff + filterVal * fresnelReflectance;
             refractContribut = filterVal * (1.0f - fresnelReflectance);
@@ -91,13 +102,14 @@ glm::vec3 raytrace(glm::vec3 p0, glm::vec3 d, float *tPassBack, std::vector<Geom
 
             reflectColor = glm::vec3(0, 0, 0);
             refractionColor = glm::vec3(0, 0, 0);
-            if (reflectCoeff > 0) {
+            //if obj doesn't have reflection or refract don't cast those rays
+            if (reflectCoeff > 0 || filterVal > 0) {
                 reflectColor = raytrace(intersectionPt + reflectionVec * epsilon, reflectionVec, &reflectT, objList, lightList, depth - 1, printMode, "Relfection", altBRDF, useFresnel);
                 if (std::isnan(reflectColor.x)) {
                     reflectColor = glm::vec3(0, 0, 0);
                 }
             }
-            if (refractCoeff > 0) {
+            if (refractCoeff > 0 || filterVal > 0) {
                 refractionColor = raytrace(refractPt, refractionVec, &refractT, objList, lightList, depth - 1, printMode, "Refraction", altBRDF, useFresnel);
                 if (std::isnan(refractionColor.x)) {
                     refractionColor = glm::vec3(0, 0, 0);
